78e4aa4b676ae8e64aaab9b90dc91158d5470aa7
Dan Crooks <danielplcrooks@gmail.com>
2022-02-24 21:03:16 +0000

    sfkhjsbefj

--- solve.py
+++ solve.py
@@ -18,9 +18,11 @@ class Project:
         return self.time_worked == self.days
 
     def assign_workers(self, workers):
+        self.workers = []
         for worker, skill in workers:
             worker.working = True
             worker.skill_working_on = skill
+            self.workers.append(worker)
 
     def __init__(self, name, days, score, best, roles, skills):
         self.name = name
@@ -48,7 +50,7 @@ def read_data(filename):
                 skills[skill_name] = level
             contributors.append(Contributor(contrib_name, skills))
         projects = []
-        for _ in range(P):
+        for p in range(P):
             line = in_file.readline().strip().split(" ")
             name = line[0]
             D, S, B, R = map(int, line[1:])
@@ -56,7 +58,7 @@ def read_data(filename):
             s = set()
             for _ in range(R):
                 skill_name, level = in_file.readline().strip().split(" ")
-                assert skill_name not in s, "rip"
+                assert skill_name not in s, f'{name}{p}{s}{skill_name}'
                 s.add(skill_name)
                 level = int(level)
                 skills.append((skill_name, level))
@@ -71,40 +73,51 @@ def get_next_possible_projects(workers, projects, day_counter):
     people_set = set()
 
     for project in projects:
-        if project.time_worked > 0 or project.best > day_counter:
+        print(project.name)
+        if project.time_worked > 0 or project.best < day_counter:
+            print('floop')
             continue
         # do we have bois for problem
         work_pair = [[worker, True] for worker in workers]
         picked_list = []
         for skill, level in project.skills:
+            print('a', skill, level)
             picked = False
             picked_i = 0
             for i, (worker, available) in enumerate(work_pair):
+                print('b',worker.skills, available)
                 if not available or worker in people_set:
                     continue
                 if skill in worker.skills:
                     if worker.skills[skill] >= level:
                         picked = True
                         picked_i = i
+                        break
             if picked:
                 work_pair[picked_i][1] = False
                 picked_list.append((worker, skill))
+            print(picked_list)
+        print(project.name, len(project.skills))
         if len(picked_list) == len(project.skills):
             people_set.update([worker for worker, _ in picked_list])
             people.append(picked_list)
             output_projects.append(project)
+        else:
+            print('wtf')
 
-    return projects, people
+    return output_projects, people
 
 
 def update(projects):
     p = []
+    done=[]
     for proj in projects:
-        if proj.is_done:
+        if proj.is_done():
+            done.append(proj)
             for worker in proj.workers:
                 # update their skills
                 if (
-                    proj.sneaky_skills[worker.skil_working_on]
+                    proj.sneaky_skills[worker.skill_working_on]
                     == worker.skills[worker.skill_working_on]
                 ):
                     worker.skills[worker.skill_working_on] += 1
@@ -112,13 +125,14 @@ def update(projects):
         else:
             proj.time_worked += 1
             p.append(proj)
-    return p
+    return p, done
 
 
 def solve(contrib, proj):
     day_counter = 0
     sorted_proj = sorted(proj, key=lambda x: x.rating, reverse=True)
     projects_being_worked_on = []
+    output=[]
     while True:
         print("boop")
         projects, people = get_next_possible_projects(contrib, sorted_proj, day_counter)
@@ -131,12 +145,19 @@ def solve(contrib, proj):
             projects_being_worked_on.append(project)
 
         print(projects_being_worked_on)
-        projects_being_worked_on = update(projects_being_worked_on)
+        projects_being_worked_on, projects_done = update(projects_being_worked_on)
+        for project in projects_done:
+            output.append([project.name]+project.workers)
         day_counter += 1
-
+    return output
 
 def write_file(filename, answer):
-    pass
+    with open(f'output_data/{filename}', 'w') as out_file:
+        out_file.write(str(len(answer))+'\n')
+        for n in answer:
+            out_file.write(n[0]+'\n')
+            out_file.write(' '.join([a.name for a in n[1:]])+'\n')
+
 
 
 if __name__ == "__main__":
@@ -145,10 +166,8 @@ if __name__ == "__main__":
         print(f"Reading file {input_file}")
 
         contributors, projects = read_data(input_file)
-        print(contributors)
-        print(projects)
 
         answer = solve(contributors, projects)
         print(answer)
-        assert False, "sweet jesus we did it"
-        write_file(input_file.split(".")[0] + ".out", answer)
+        #assert False, "sweet jesus we did it"
+        write_file(input_file.name.split('.')[0] + ".out", answer)
