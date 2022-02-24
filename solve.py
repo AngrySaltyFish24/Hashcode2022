875168a06f341a8d4a718ee8199930dd50ee3db3
James Rand <jamesrand@protonmail.com>
2022-02-24 20:10:19 +0000

    lskdjfkls

--- solve.py
+++ solve.py
@@ -1,9 +1,5 @@
-from audioop import reverse
-from calendar import day_abbr
-from distutils.file_util import write_file
-from logging import _Level
-import sys
 import pathlib
+
 
 class Contributor:
     def __init__(self, name, skills):
@@ -35,64 +31,68 @@ class Project:
         self.skills = skills
         self.time_worked = 0
         self.workers = []
-        self.sneaky_skills = {a:b for a, b in skills}
+        self.sneaky_skills = {a: b for a, b in skills}
 
 
 def read_data(filename):
     with open(f"{filename}", "r") as in_file:
         C, P = map(int, in_file.readline().strip().split(" "))
         contributors = []
-        for c in range(C):
+        for _ in range(C):
             contrib_name, N = in_file.readline().strip().split(" ")
             N = int(N)
             skills = {}
-            for n in range(N):
+            for _ in range(N):
                 skill_name, level = in_file.readline().strip().split(" ")
                 level = int(level)
                 skills[skill_name] = level
-        contributors.append(Contributor(contrib_name, skills))
+            contributors.append(Contributor(contrib_name, skills))
         projects = []
-        for p in range(P):
+        for _ in range(P):
             line = in_file.readline().strip().split(" ")
             name = line[0]
             D, S, B, R = map(int, line[1:])
-            skills=[]
+            skills = []
             s = set()
-            for r in range(R):
+            for _ in range(R):
                 skill_name, level = in_file.readline().strip().split(" ")
-                assert skill_name not in s, 'rip'
+                assert skill_name not in s, "rip"
                 s.add(skill_name)
                 level = int(level)
                 skills.append((skill_name, level))
             projects.append(Project(name, D, S, B, R, skills))
     return contributors, projects
 
-def get_next_possible_projects(workers, projects):
-    #return list of projects and list of lists of workers
-    projects = []
-    people=[]
+
+def get_next_possible_projects(workers, projects, day_counter):
+    # return list of projects and list of lists of workers
+    output_projects = []
+    people = []
+    people_set = set()
+
     for project in projects:
-        if project.time_worked > 0:
+        if project.time_worked > 0 or project.best > day_counter:
             continue
         # do we have bois for problem
         work_pair = [[worker, True] for worker in workers]
-        picked_list=[]
+        picked_list = []
         for skill, level in project.skills:
-            picked=False
-            picked_i=0
+            picked = False
+            picked_i = 0
             for i, (worker, available) in enumerate(work_pair):
-                if not available:
+                if not available or worker in people_set:
                     continue
                 if skill in worker.skills:
-                    if worker.skills[skill]>=level:
-                        picked=True
-                        picked_i=i
+                    if worker.skills[skill] >= level:
+                        picked = True
+                        picked_i = i
             if picked:
                 work_pair[picked_i][1] = False
                 picked_list.append((worker, skill))
         if len(picked_list) == len(project.skills):
+            people_set.update([worker for worker, _ in picked_list])
             people.append(picked_list)
-            projects.append(project)
+            output_projects.append(project)
 
     return projects, people
 
@@ -101,9 +101,12 @@ def update(projects):
     p = []
     for proj in projects:
         if proj.is_done:
-            for worker in proj.workers():
-                #update their skills
-                if proj.sneaky_skills[worker.skil_working_on] == worker.skills[worker.skill_working_on]:
+            for worker in proj.workers:
+                # update their skills
+                if (
+                    proj.sneaky_skills[worker.skil_working_on]
+                    == worker.skills[worker.skill_working_on]
+                ):
                     worker.skills[worker.skill_working_on] += 1
                 worker.working = False
         else:
@@ -111,11 +114,16 @@ def update(projects):
             p.append(proj)
     return p
 
+
 def solve(contrib, proj):
+    day_counter = 0
     sorted_proj = sorted(proj, key=lambda x: x.rating, reverse=True)
-    projects_being_worked_on=[]
+    projects_being_worked_on = []
     while True:
-        projects, people = get_next_possible_projects(contrib, sorted_proj)
+        print("boop")
+        projects, people = get_next_possible_projects(contrib, sorted_proj, day_counter)
+        print(f" printing projects {projects}")
+        print(f" printing people {people}")
         if projects == [] and projects_being_worked_on == []:
             break
         for project, workers in zip(projects, people):
@@ -123,11 +131,12 @@ def solve(contrib, proj):
             projects_being_worked_on.append(project)
 
         projects_being_worked_on = update(projects_being_worked_on)
-
+        day_counter += 1
 
 
 def write_file(filename, answer):
     pass
+
 
 if __name__ == "__main__":
     input_folder = pathlib.Path("input_data")
@@ -135,9 +144,10 @@ if __name__ == "__main__":
         print(f"Reading file {input_file}")
 
         contributors, projects = read_data(input_file)
+        print(contributors)
+        print(projects)
 
         answer = solve(contributors, projects)
-        assert False, 'sweet jesus we did it'
-        write_file(input_file.split('.')[0]+'.out', answer)
-
-
+        print(answer)
+        assert False, "sweet jesus we did it"
+        write_file(input_file.split(".")[0] + ".out", answer)
