5beaa09758718f6b981fa10f693ca8efa19815f2
James Rand <jamesrand@protonmail.com>
2022-02-24 18:26:02 +0000

    slkdjfdslk

--- solve.py
+++ solve.py
@@ -1,12 +1,18 @@
+import sys
+import pathlib
+
+
 class Skill:
     def __init__(self, name, level):
         self.name = name
         self.level = level
 
+
 class Contributor:
     def __init__(self, name, skills):
         self.name = name
         self.skills = skills
+
 
 class Project:
     def __init__(self, days, score, best, roles):
@@ -18,21 +24,21 @@ class Project:
 
 
 def read_data(filename):
-    with open(f'input_data/{filename}', 'r') as in_file:
-        C, P = map(int, in_file.readline().strip().split(' '))
+    with open(f"{filename}", "r") as in_file:
+        C, P = map(int, in_file.readline().strip().split(" "))
         contributors = []
         for c in range(C):
-            contrib_name, N = in_file.readline().strip().split(' ')
+            contrib_name, N = in_file.readline().strip().split(" ")
             N = int(N)
             skills = []
             for n in range(N):
-                skill_name, level = in_file.readline().strip().split(' ')
+                skill_name, level = in_file.readline().strip().split(" ")
                 level = int(level)
                 skills.append(Skill(skill_name, level))
         contributors.append(Contributor(contrib_name, skills))
         projects = []
         for p in range(P):
-            line = in_file.readline().strip().split(' ')
+            line = in_file.readline().strip().split(" ")
             name = line[0]
             D, S, B, R = map(int, line[1:])
             projects.append(Project(name, D, S, B, R))
@@ -40,3 +46,8 @@ def read_data(filename):
 
 
 if __name__ == "__main__":
+    input_folder = pathlib.Path("input_data")
+    for input_file in input_folder.iterdir():
+        print(f"Reading file {input_file}")
+
+        read_data(input_file)
